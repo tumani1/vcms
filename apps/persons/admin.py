0682c24185e77b055bb01e28dbb958487fe44147
tumani1 <tumani1@yandex.ru>
2014-03-26 19:22:26 +0400

    - правки admin.py

--- apps/persons/admin.py
+++ apps/persons/admin.py
@@ -12,29 +12,12 @@ class PersonsAdmin(admin.ModelAdmin):
 
 
 #############################################################################################################
-# Администрирование таблицы связей персон и тем
-class PersonsTopicsAdmin(admin.ModelAdmin):
-    search_fields = ('id',)
-
-
-#############################################################################################################
 # Администрирование таблицы дополнительных материалов персон
 class PersonsExtrasAdmin(admin.ModelAdmin):
     search_fields = ('id',)
 
 
 #############################################################################################################
-# Администрирование таблицы расширения
-class UsersPersonsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'person', 'topic', 'rating', 'status', 'updated',)
-    list_filter = ('status', 'updated',)
-    search_fields = ('user', )
-    raw_id_fields = ('user', 'person', 'topic',)
-
-
-#############################################################################################################
 # Регистрация моделей в админке
 admin.site.register(Persons, PersonsAdmin)
-admin.site.register(PersonsTopics, PersonsTopicsAdmin)
 admin.site.register(PersonsExtras,PersonsExtrasAdmin)
-admin.site.register(UsersPersons, UsersPersonsAdmin)
