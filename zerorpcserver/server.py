74e50f4b7de2a956b03bb1a0926b1ea3ef78e628
Denis Lyubimov <dlyubimov@aaysm.com>
2014-06-03 15:13:21 +0400

    Changed server to pure echo

--- zerorpcserver/server.py
+++ zerorpcserver/server.py
@@ -4,7 +4,7 @@ import zerorpc
 
 class HelloRPC(object):
     def routing(self, json_data):
-        return "{} !!!".format(json_data.upper())
+        return "{}".format(json_data)
 
 server = zerorpc.Server(HelloRPC())
 server.bind("tcp://0.0.0.0:4242")
