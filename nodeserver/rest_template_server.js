6c684f6b665b05ae525391205e607e06554faedb
Krasnov Dmitriy <dkrasnov@aaysm.com>
2014-06-04 18:33:53 +0400

    убран парсинг и парсер из zerorpc службы, подправлена RTnode служба

--- nodeserver/rest_template_server.js
+++ nodeserver/rest_template_server.js
@@ -10,15 +10,14 @@ function validate(vurl) {
     return re.exec(vurl);  // длинна массива соотвествует количеству групп в regexp +1
 }
 
-function form_ipc_pack(request, directives, query_params) {
-    // request нужен, чтобы взять параметры при типе запроса PUT/POST
-    qp = querystring.parse(query_params);
+function form_ipc_pack(directives, method, query_params) {
+    var qp = querystring.parse(query_params);
     qp["ident"] = (directives[2] ? directives[2].slice(1) : null);
 
-    return JSON.stringify({api_group: directives[1],
-                           api_method: directives[3],
-                           http_method: request.method,
-                           query_params: qp});
+    return {api_group: directives[1],
+            api_method: directives[3],
+            http_method: method,
+            query_params: qp};
 }
 
 function run_server(host, port) {  // якобы общепринятое правило прятать всё в функцию
@@ -28,9 +27,22 @@ function run_server(host, port) {  // якобы общепринятое пра
     var server = http.createServer(function(request, response) {
         var parsed = url.parse(request.url);
         var vurl = parsed.pathname, query_params = parsed.query;
+        var meth = request.method;
         var directives = validate(vurl);
-        if (directives != null) {
-            var IPC_pack = form_ipc_pack(request, directives, query_params);
+        var IPC_pack;
+        if (meth in ["POST", "PUT"] && directives != null) {
+            IPC_pack = form_ipc_pack(directives, meth, query_params);
+            log("received IPC: " + IPC_pack.toString());
+            request.on('data', function(chunk) {
+                IPC_pack["query_params"] = null;
+                client.invoke("route", IPC_pack, function(error, res, more) {
+                    response.writeHead(200, {"Content-Type": "text/plain"});
+                    response.end(res);
+                })
+            });
+        }
+        else if (directives != null) {
+            IPC_pack = form_ipc_pack(directives, meth, query_params);
             log("received IPC: " + IPC_pack.toString());
             client.invoke("route", IPC_pack, function(error, res, more) {
                 response.writeHead(200, {"Content-Type": "text/plain"});
