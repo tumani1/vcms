2402363a57621167e21f18284198c47cdac905d9
Denis Lyubimov <dlyubimov@aaysm.com>
2014-06-04 14:38:44 +0400

    Added raven/sentry integration

--- zerorpcserver/server.py
+++ zerorpcserver/server.py
@@ -4,10 +4,30 @@ import zerorpc
 from api import routes
 from api import authorize
 import ujson
+from raven import Client
+
+DEBUG = True
+
+def raven_report(func):
+    if DEBUG:
+        return func
+    else:
+        client = Client('http://5aec720be5594c3e8c4e456ec8f8523a:6d461d2eecce47c281c052cff0ec8a63@sentry.aaysm.com/3')
+        
+        def wrapper(self,json_data):
+            try:
+                return func(self,json_data)
+            except Exception:
+                client.captureException()
+        return wrapper    
+
 
 mashed_routes = dict( ((g,a,h),routes[g][a][h]) for g in routes for a in routes[g] for h in routes[g][a])
 
+
+
 class HelloRPC(object):
+    @raven_report
     def routing(self, json_data):
 
         pd = ujson.loads(json_data)
