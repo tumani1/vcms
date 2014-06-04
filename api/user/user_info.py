4e54cce7df2ec6f760e81c5c5238f560e089c1df
sergey.i <sipatov@aaysm.com>
2014-06-04 18:38:52 +0400

    user_info get

---
+++ api/user/user_info.py
@@ -0,0 +1,20 @@
+from models import db, Users, Cities, Countries
+
+
+@db
+def get(user_id, session= None):
+    user = session.query(Users).filter_by(id=user_id).first()
+    city = session.query(Cities).filter_by(id=user.city_id).first()
+    country = session.query(Countries).filter_by(id=city.country_id).first()
+    result = {
+        'id': user.id,
+        'firstname': user.firstname,
+        'lastname': user.lastname,
+        'userpic': user.firstname,
+        'time_zone': user.time_zone,
+        'country': country.name,
+        'city': city.name,
+    }
+    return result
+@db
+def put(user_id, session=None,**kwargs):
