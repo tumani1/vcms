6dccb1af5d1f0583a7212de3b8b32ab74e5aa9b4
Denis Lyubimov <dlyubimov@aaysm.com>
2014-06-04 10:59:29 +0400

    Added test routine

--- api/__init__.py
+++ api/__init__.py
@@ -2,11 +2,13 @@
 from users import routing as users_routing
 from topics import routing as topics_routing
 from persons import routing as persons_routing
+from test import routes as test_routing
 
 routes = {
     'users': users_routing,
     'topics': topics_routing,
-    'persons' : persons_routing
+    'persons' : persons_routing,
+    'test':test_routing
 }
 
 def authorize(token):
---
+++ api/test/__init__.py
@@ -0,0 +1,6 @@
+
+
+
+routes = {'echo':
+          {'put':lambda message:message}
+}
