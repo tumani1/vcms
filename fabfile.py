297a4ff14c3fb55b445125896d5da4043cba44cd
Denis Lyubimov <dlyubimov@aaysm.com>
2014-06-05 20:18:04 +0400

    Fixed db reset issue

--- fabfile.py
+++ fabfile.py
@@ -6,5 +6,5 @@ def local_db_reset():
     Перезаписать локальную базу из репозитория
     '''
     local('''echo "DROP DATABASE next_tv;" |  sudo  sudo -u postgres psql''')
-    local('''echo "CREATE USER pgadmin WITH PASSWORD 'qwerty'; CREATE DATABASE next_tv; GRANT ALL PRIVILEGES ON DATABASE videobase to pgadmin;" | sudo  sudo -u postgres psql''')
+    local('''echo "CREATE USER pgadmin WITH PASSWORD 'qwerty'; CREATE DATABASE next_tv; GRANT ALL PRIVILEGES ON DATABASE next_tv to pgadmin;" | sudo  sudo -u postgres psql''')
 
