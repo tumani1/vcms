9e2cb6501a1e312e36d2344e9089ccaf47e59cd1
Denis Lyubimov <dlyubimov@aaysm.com>
2014-06-05 17:52:47 +0400

    Fixed shell in fabfile

--- fabfile.py
+++ fabfile.py
@@ -5,6 +5,6 @@ def local_db_reset():
     '''
     Перезаписать локальную базу из репозитория
     '''
-    local('''echo "DROP DATABASE next_tv;" |  sudo  sudo -u postgres psql''', shell = True)
-    local('''echo "CREATE USER pgadmin WITH PASSWORD 'qwerty'; CREATE DATABASE next_tv; GRANT ALL PRIVILEGES ON DATABASE videobase to pgadmin;" | sudo  sudo -u postgres psql''', shell =True)
+    local('''echo "DROP DATABASE next_tv;" |  sudo  sudo -u postgres psql''')
+    local('''echo "CREATE USER pgadmin WITH PASSWORD 'qwerty'; CREATE DATABASE next_tv; GRANT ALL PRIVILEGES ON DATABASE videobase to pgadmin;" | sudo  sudo -u postgres psql''')
 
