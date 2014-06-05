e49412fa0f3081d40a20cb76764f30962a27f9c5
Eugene <eshangin@aaysm.com>
2014-06-05 14:30:53 +0400

    user_password метод put для смены пороля

--- api/user/user_password.py
+++ api/user/user_password.py
@@ -4,4 +4,8 @@ from models import db, Users
 @db
 def put(user_id, password, session=None):
     user = session.query(Users).filter_by(id=user_id).first()
+    user.password = password
+    session.commit()
+
+
 
