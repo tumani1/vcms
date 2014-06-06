08c46195ef127a8b4adf48a0a4ca3855cdc8901b
Ivan <ikrivosheev@aaysm.com>
2014-06-06 16:19:15 +0400

    Add fake api to users

--- api/users/__init__.py
+++ api/users/__init__.py
@@ -1,10 +1,28 @@
-from users_friendship_action import get, post, delete
+import users_friendship_action
+import users_info
+import users_values
+import users_friends
 
-friendship = {'get': get,
-              'post': post,
-              'delete': delete,
+friendship = {'get': users_friendship_action.get,
+              'post': users_friendship_action.post,
+              'delete': users_friendship_action.delete,
+}
+
+info = {
+    'get': users_info.get,
+}
+
+values = {
+    'get': users_values.get,
+}
+
+friends = {
+    'get': users_friends.get
 }
 
 
 routing = { 'friendship': friendship,
+            'info': info,
+            'values': values,
+            'friends': friends,
 }
--- api/users/users_values.py
+++ api/users/users_values.py
@@ -2,3 +2,6 @@
 from models import db
 
 
+@db
+def get(user_id, session=None, **kwargs):
+    pass
