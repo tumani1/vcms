e44bae0521a9e0dd8977e4149c1af9a3cbb30c3d
Krasnov Dmitriy <dkrasnov@aaysm.com>
2014-06-09 13:19:39 +0400

    имена типов методов приведены к нижнему регистру

--- example.sh
+++ example.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Example of client request for our backend zerorpc service
-zerorpc --json --client tcp://0.0.0.0:4242 route '{"api_group": "persons", "api_method":"info", "api_format":"json", "token":"echo_token", "http_method":"GET", "query_params":{"person":"1"}}'
+zerorpc --json --client tcp://0.0.0.0:4242 route '{"api_group": "persons", "api_method":"info", "api_format":"json", "token":"echo_token", "http_method":"get", "query_params":{"person":"1"}}'
