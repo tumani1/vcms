176ae415e8adbec7a1a5eec20a51fab6a5cb1475
Eugene <eshangin@aaysm.com>
2014-06-17 17:05:18 +0400

    msgr_create

--- api/msgr/msgr_create.py
+++ api/msgr/msgr_create.py
@@ -1,4 +1,6 @@
+import datetime
 from requests import Response
+from sqlalchemy import Date
 from api.users.serializer import mUserShort
 from models import db, MsgrThreads, UsersMsgrThreads, MsgrLog, Users
 from utils import need_authorization
@@ -7,12 +9,20 @@ from utils import need_authorization
 @db
 @need_authorization
 def put(user, session=None, **kwargs):
-    if ('attachments' in kwargs or 'text' in kwargs):
-        return ''
-    users = session.query.filter(Users.id.in_(kwargs['user_ids']))
-    user_msgr_threads = session.query(UsersMsgrThreads).filter_by(msgr_threads_id=kwargs['user_ids']).first()
-    msgr_thread = session.query(MsgrThreads).filter_by(id=user_msgr_threads.msgr_threads_id).first()
-    ml = MsgrLog(msgr_threads_id=msgr_thread.id, user_id=user_msgr_threads.user_id, text=kwargs['text'])
+    # if ('attachments' in kwargs or 'text' in kwargs):
+    #     return ''
+    mt = MsgrThreads(msg_cnt=1)
+    session.add(mt)
+    session.commit()
+    users = session.query(Users).filter(Users.id.in_(kwargs['user_ids']))
+    for id in kwargs['user_ids']:
+        user_msgr_threads = UsersMsgrThreads(user_id=id, msgr_threads_id=mt.id, new_msgs=1)
+        session.add(user_msgr_threads)
+    date = datetime.datetime.now()
+    user_msgr_threads = UsersMsgrThreads(user_id=user.id, msgr_thread_id=mt.id, last_msg_sent=date, last_visit=date, new_msgs=0)
+    session.add(user_msgr_threads)
+
+    ml = MsgrLog(msgr_threads_id=mt.id, user_id=user.id, text=kwargs['text'])
     session.add(ml)
     params = {
         'user': user,
