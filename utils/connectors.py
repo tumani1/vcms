4e8c5b774d821d7890d814263b39f6fef0ba4b0a
tumani1 <itumanov@aaysm.com>
2014-06-18 03:44:20 +0400

    -refactor соединения к базе

--- utils/connectors.py
+++ utils/connectors.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-from sqlalchemy import create_engine, pool
+from sqlalchemy import create_engine
 from sqlalchemy.engine.url import URL
 from sqlalchemy.orm import sessionmaker, scoped_session
 
@@ -18,27 +18,22 @@ def db_connect(type='postgresql', **kwargs):
     return create_engine(URL(**db_settings), **kwargs)
 
 
+# Function which create session marker
+def create_session(**kwargs):
+    return scoped_session(sessionmaker(**kwargs))()
+
+
 # Class decorator for to the database
 class DBWrapper(object):
-    def __init__(self, engine=None, poolclass=None):
-        poolclass = poolclass or pool.SingletonThreadPool
+    def __init__(self, engine, session):
 
-        if engine is None:
-            self.engine = db_connect(poolclass=poolclass)
-        else:
-            self.engine = engine
+        self.engine = engine
+        self.session = session
 
 
     def __call__(self, func):
-        def wrapper(*args,**kwargs):
-            # Init params
-            params = {
-                'bind': self.engine,
-                'expire_on_commit': False,
-            }
-
-            # Init Session
-            session = scoped_session(sessionmaker(**params))()
+        def wrapper(*args, **kwargs):
+            session = self.session
 
             try:
                 return func(session=session, *args, **kwargs)
