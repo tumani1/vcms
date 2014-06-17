c7daf0fa9718a1c4f7c5d2df8575a556fdebdd1f
Eugene <eshangin@aaysm.com>
2014-06-17 15:17:07 +0400

    msgr_create

--- api/msgr/msgr_create.py
+++ api/msgr/msgr_create.py
@@ -1,5 +1,6 @@
 from requests import Response
-from models import db, MsgrThreads, UsersMsgrThreads, MsgrLog
+from api.users.serializer import mUserShort
+from models import db, MsgrThreads, UsersMsgrThreads, MsgrLog, Users
 from utils import need_authorization
 
 
@@ -7,9 +8,23 @@ from utils import need_authorization
 @need_authorization
 def put(user, session=None, **kwargs):
     if ('attachments' in kwargs or 'text' in kwargs):
-        return Response.status_code
+        return ''
+    users = session.query.filter(Users.id.in_(kwargs['user_ids']))
     user_msgr_threads = session.query(UsersMsgrThreads).filter_by(msgr_threads_id=kwargs['user_ids']).first()
     msgr_thread = session.query(MsgrThreads).filter_by(id=user_msgr_threads.msgr_threads_id).first()
     ml = MsgrLog(msgr_threads_id=msgr_thread.id, user_id=user_msgr_threads.user_id, text=kwargs['text'])
     session.add(ml)
+    params = {
+        'user': user,
+        'session': session,
+        'instance': users
+    }
+
     session.commit()
+    m_user_short = mUserShort(**params).data
+    result = {
+        'id': "",
+        'msgr_cnt': '',
+        'users': m_user_short
+    }
+    return result
