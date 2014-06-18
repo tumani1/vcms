b6e0f095da10e4bf2b88812f4e5904e89edab81c
Krasnov Dmitriy <dkrasnov@aaysm.com>
2014-06-18 18:21:19 +0400

    удалён fabfile.py, его код перенесён в manage.py

--- manage.py
+++ manage.py
@@ -1,12 +1,17 @@
-
 # coding: utf-8
 from argparse import ArgumentParser
-
 from admin.main import start_admin_application
 from zerorpcserver.server import start_zerorpc_service
 from models import Base
 from utils.connectors import db_connect
+from fabric.api import local
 
+
+def local_db_reset():
+    '''Перезаписать локальную базу из репозитория
+    '''
+    local('''echo "DROP DATABASE next_tv;" |  sudo  sudo -u postgres psql''')
+    local('''echo "CREATE USER pgadmin WITH PASSWORD 'qwerty'; CREATE DATABASE next_tv; GRANT ALL PRIVILEGES ON DATABASE next_tv to pgadmin;" | sudo  sudo -u postgres psql''')
 
 def delete_tables(**options):
     Base.metadata.drop_all(bind=db_connect())
@@ -31,6 +36,9 @@ if __name__ == '__main__':
     admin_c.add_argument('--debug', dest='debug', action='store_true', help='Run admin in debug mode')
     admin_c.add_argument('--no-debug', dest='debug', action='store_false', help='Run admin without debug mode')
     admin_c.set_defaults(func=start_admin_application, debug=True)
+
+    local_db_reset_c = subparser.add_parser('local_db_reset', help='Сносит БД, создает пользователя и новую БД, добавляет прва пользователю на эту БД')
+    local_db_reset_c.set_defaults(func=local_db_reset)
 
     syncdb_c = subparser.add_parser('syncdb', help='Create tabels')
     syncdb_c.set_defaults(func=create_tables)
