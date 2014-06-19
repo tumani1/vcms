d177812f97b05ac1bf1b6b128130e24b4c58aca6
tumani1 <itumanov@aaysm.com>
2014-06-19 15:21:18 +0400

    - шаблон тестов топиков

--- models/tokens/session_token.py
+++ models/tokens/session_token.py
@@ -13,47 +13,53 @@ class SessionToken(TokenMixin):
 
     is_active = Column(Boolean, default=True)
 
+
     @classmethod
     def get_user_id_by_token(cls, token_string, session=None):
-
         st = session.query(SessionToken).filter(cls.token == token_string).first()
 
-        if st is None:
-            return None
-        else:
+        if not st is None:
             if (datetime.datetime.utcnow() - st.created < datetime.timedelta(minutes=TOKEN_LIFETIME)) and st.is_active:
                 return st.user_id
             else:
                 st.is_active = False
                 session.add(st)
                 session.commit()
-                return None
+
+        return None
+
 
     @classmethod
     def user_is_online(cls, user_id, session=None):
         sess = session.query(cls).filter_by(user_id=user_id).order_by(cls.created.desc()).first()
         return sess and sess.is_active and (datetime.datetime.utcnow() - sess.created < datetime.timedelta(minutes=TOKEN_LIFETIME))
 
+
     @classmethod
     def filter_users_is_online(cls, is_online, query):
         if is_online:
-            query = query.join(cls).filter(cls.is_active, cls.created - datetime.datetime.utcnow() < datetime.timedelta(minutes=TOKEN_LIFETIME))
+            query = query.join(cls).filter(
+                cls.is_active,
+                cls.created - datetime.datetime.utcnow() < datetime.timedelta(minutes=TOKEN_LIFETIME)
+            )
         else:
-            query = query.join(cls).filter(or_(cls.is_active == False, cls.created - datetime.datetime.utcnow() >= datetime.timedelta(minutes=TOKEN_LIFETIME)))
+            query = query.join(cls).filter(or_(
+                cls.is_active == False,
+                cls.created - datetime.datetime.utcnow() >= datetime.timedelta(minutes=TOKEN_LIFETIME)
+            ))
 
         return query
 
 
-
     @classmethod
-    def generate_token(cls,user_id,session):
-
+    def generate_token(cls, user_id, session):
         '''
         (Re)Generate token for given user_id
         '''
 
-        st = SessionToken(user_id=user_id,is_active=True)
+        st = SessionToken(user_id=user_id, is_active=True)
         
         session.add(st)
         session.commit()
-        return st.id,st.token,st.created
+
+        return st.id, st.token, st.created
--- tests/test_topic.py
+++ tests/test_topic.py
@@ -4,44 +4,343 @@ import zerorpc
 import unittest
 import datetime
 
-from create_test_user import create_topic
+from create_test_user import create
+from models import SessionToken, Topics, db
 
 
-class TopicTestCase(unittest.TestCase):
+@db
+def create_topic(session):
+    topic = Topics(name="test11", title="test", description="test test", releasedate=datetime.datetime(2014,1,1,0,0,0), status='a', type='news')
+    session.add(topic)
+    session.commit()
 
-    def setUp(self):
-        self.cl = zerorpc.Client()
-        self.cl.connect("tcp://127.0.0.1:4242")
-        create_topic()
+    return topic
 
-    def test_echo(self):
-        IPC_pack = {
-            "api_group": "topics",
-            "api_method":"info",
-            "api_format":"json",
-            "token":"echo_token",
-            "http_method":"get",
-            "query_params":{
-                "name": "test"
-            }
-        }
 
-        resp = self.cl.route(IPC_pack)
-        temp = {
-            'name': 'test11',
-            'title': 'test',
-            'title_orig': None,
-            'description': 'test test',
-            'releasedate': datetime.datetime(2014,1,1,0,0,0),
-            'type': 'news',
-            'relation': {
-                'subscribed': False,
-                'liked': 0,
-            }
-        }
-
-        print resp
-        self.assertEqual(temp, resp)
-
-    def tearDown(self):
-        self.cl.close()
+# ###################################################################################
+# class TopicInfoTestCase(unittest.TestCase):
+#
+#     def setUp(self):
+#         self.cl = zerorpc.Client(timeout=300)
+#         self.cl.connect("tcp://127.0.0.1:4242")
+#         self.topic = create_topic()
+#
+#
+#     def test_echo(self):
+#         IPC_pack = {
+#             "api_group": "topics",
+#             "api_method": "info",
+#             "api_format": "json",
+#             "http_method": "get",
+#             "query_params": {
+#                 "name": self.topic.name,
+#             }
+#         }
+#
+#         resp = self.cl.route(IPC_pack)
+#         temp = {
+#             'name': 'test',
+#             'title': 'test',
+#             'title_orig': None,
+#             'description': 'test test',
+#             'releasedate': datetime.datetime(2014, 1, 1, 0, 0, 0),
+#             'type': 'news',
+#             'relation': {
+#                 'subscribed': False,
+#                 'liked': 0,
+#             }
+#         }
+#
+#         # self.assertDictEqual(temp, resp)
+#         self.assertEqual(temp, resp)
+#
+#     def tearDown(self):
+#         self.cl.close()
+#
+#
+# ###################################################################################
+# class TopicLikeTestCase(unittest.TestCase):
+#
+#     def setUp(self):
+#         self.cl = zerorpc.Client(timeout=300)
+#         self.cl.connect("tcp://127.0.0.1:4242")
+#
+#         self.topic = create_topic()
+#
+#         self.user_id = create()
+#         self.session_token = SessionToken.generate_token(self.user_id, session=self.session)
+#
+#
+#     def test_echo_get(self):
+#         IPC_pack = {
+#             "api_group": "topics",
+#             "api_method": "like",
+#             "api_format": "json",
+#             "x_token": self.session_token[1],
+#             "http_method": "get",
+#             "query_params": {
+#                 "name": self.topic.name,
+#             }
+#         }
+#
+#         resp = self.cl.route(IPC_pack)
+#         temp = {}
+#
+#         self.assertEqual(temp, resp)
+#
+#
+#     def test_echo_post(self):
+#         IPC_pack = {
+#             "api_group": "topics",
+#             "api_method": "like",
+#             "api_format": "json",
+#             "x_token": self.session_token[1],
+#             "http_method": "post",
+#             "query_params": {
+#                 "name": self.topic.name,
+#             }
+#         }
+#
+#         resp = self.cl.route(IPC_pack)
+#         temp = {}
+#
+#         self.assertEqual(temp, resp)
+#
+#
+#     def test_echo_delete(self):
+#         IPC_pack = {
+#             "api_group": "topics",
+#             "api_method": "like",
+#             "api_format": "json",
+#             "x_token": self.session_token[1],
+#             "http_method": "delete",
+#             "query_params": {
+#                 "name": self.topic.name,
+#             }
+#         }
+#
+#         resp = self.cl.route(IPC_pack)
+#         temp = {}
+#
+#         self.assertEqual(temp, resp)
+#
+#
+#     def tearDown(self):
+#         self.cl.close()
+#
+#
+# ###################################################################################
+# class TopicSubscribeTestCase(unittest.TestCase):
+#
+#     def setUp(self):
+#         self.cl = zerorpc.Client(timeout=300)
+#         self.cl.connect("tcp://127.0.0.1:4242")
+#
+#         self.topic = create_topic()
+#
+#         self.user_id = create()
+#         self.session_token = SessionToken.generate_token(self.user_id, session=self.session)
+#
+#
+#
+#     def test_echo_get(self):
+#         IPC_pack = {
+#             "api_group": "topics",
+#             "api_method": "subscribe",
+#             "api_format": "json",
+#             "x_token": self.session_token[1],
+#             "http_method": "get",
+#             "query_params": {
+#                 "name": self.topic.name,
+#             }
+#         }
+#
+#         resp = self.cl.route(IPC_pack)
+#         temp = {}
+#
+#         self.assertEqual(temp, resp)
+#
+#
+#     def test_echo_post(self):
+#         IPC_pack = {
+#             "api_group": "topics",
+#             "api_method": "subscribe",
+#             "api_format": "json",
+#             "x_token": self.session_token[1],
+#             "http_method": "post",
+#             "query_params": {
+#                 "name": self.topic.name,
+#             }
+#         }
+#
+#         resp = self.cl.route(IPC_pack)
+#         temp = {}
+#
+#         self.assertEqual(temp, resp)
+#
+#
+#     def test_echo_delete(self):
+#         IPC_pack = {
+#             "api_group": "topics",
+#             "api_method": "subscribe",
+#             "api_format": "json",
+#             "x_token": self.session_token[1],
+#             "http_method": "delete",
+#             "query_params": {
+#                 "name": self.topic.name,
+#             }
+#         }
+#
+#         resp = self.cl.route(IPC_pack)
+#         temp = {}
+#
+#         self.assertEqual(temp, resp)
+#
+#
+#     def tearDown(self):
+#         self.cl.close()
+#
+#
+# ###################################################################################
+# class TopicExtrasTestCase(unittest.TestCase):
+#
+#     def setUp(self):
+#         self.cl = zerorpc.Client(timeout=300)
+#         self.cl.connect("tcp://127.0.0.1:4242")
+#         self.topic = create_topic()
+#
+#
+#     def test_echo(self):
+#         IPC_pack = {
+#             "api_group": "topics",
+#             "api_method": "extras",
+#             "api_format": "json",
+#             "http_method": "get",
+#             "query_params": {
+#                 "name": self.topic.name,
+#             }
+#         }
+#
+#         resp = self.cl.route(IPC_pack)
+#         temp = {}
+#
+#         self.assertEqual(temp, resp)
+#
+#     def tearDown(self):
+#         self.cl.close()
+#
+#
+# ###################################################################################
+# class TopicListTestCase(unittest.TestCase):
+#
+#     def setUp(self):
+#         self.cl = zerorpc.Client(timeout=300)
+#         self.cl.connect("tcp://127.0.0.1:4242")
+#         self.topic = create_topic()
+#
+#
+#     def test_echo(self):
+#         IPC_pack = {
+#             "api_group": "topics",
+#             "api_method": "list",
+#             "api_format": "json",
+#             "http_method": "get",
+#             "query_params": {
+#                 "name": self.topic.name,
+#             }
+#         }
+#
+#         resp = self.cl.route(IPC_pack)
+#         temp = {}
+#
+#         self.assertEqual(temp, resp)
+#
+#     def tearDown(self):
+#         self.cl.close()
+#
+#
+# ###################################################################################
+# class TopicValuesTestCase(unittest.TestCase):
+#
+#     def setUp(self):
+#         self.cl = zerorpc.Client(timeout=300)
+#         self.cl.connect("tcp://127.0.0.1:4242")
+#         self.topic = create_topic()
+#
+#
+#     def test_echo(self):
+#         IPC_pack = {
+#             "api_group": "topics",
+#             "api_method": "values",
+#             "api_format": "json",
+#             "http_method": "get",
+#             "query_params": {
+#                 "name": self.topic.name,
+#             }
+#         }
+#
+#         resp = self.cl.route(IPC_pack)
+#         temp = {}
+#
+#         self.assertEqual(temp, resp)
+#
+#     def tearDown(self):
+#         self.cl.close()
+#
+#
+# ###################################################################################
+# class TopicMediaTestCase(unittest.TestCase):
+#
+#     def setUp(self):
+#         self.cl = zerorpc.Client(timeout=300)
+#         self.cl.connect("tcp://127.0.0.1:4242")
+#         self.topic = create_topic()
+#
+#
+#     def test_echo(self):
+#         IPC_pack = {
+#             "api_group": "topics",
+#             "api_method": "media",
+#             "api_format": "json",
+#             "http_method": "get",
+#             "query_params": {
+#                 "name": self.topic.name,
+#             }
+#         }
+#
+#         resp = self.cl.route(IPC_pack)
+#         temp = {}
+#
+#         self.assertEqual(temp, resp)
+#
+#     def tearDown(self):
+#         self.cl.close()
+#
+#
+# ###################################################################################
+# class TopicPersonsTestCase(unittest.TestCase):
+#
+#     def setUp(self):
+#         self.cl = zerorpc.Client(timeout=300)
+#         self.cl.connect("tcp://127.0.0.1:4242")
+#         self.topic = create_topic()
+#
+#
+#     def test_echo(self):
+#         IPC_pack = {
+#             "api_group": "topics",
+#             "api_method": "persons",
+#             "api_format": "json",
+#             "http_method": "get",
+#             "query_params": {
+#                 "name": self.topic.name,
+#             }
+#         }
+#
+#         resp = self.cl.route(IPC_pack)
+#         temp = {}
+#
+#         self.assertEqual(temp, resp)
+#
+#     def tearDown(self):
+#         self.cl.close()
