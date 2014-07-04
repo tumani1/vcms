e5d0e5193e1e184c45903bee100d6ee2b359c441
Ivan <ikrivosheev@aaysm.com>
2014-07-04 13:17:45 +0400

    Add kwargs for api method

--- api/auth/revoke.py
+++ api/auth/revoke.py
@@ -2,11 +2,10 @@
 from models import GlobalToken
 
 
-def revoke(auth_user, x_token, token, session):
+def revoke(auth_user, x_token, token, session, **kwargs):
     '''
     Revoking access for users Global Token by deleting it
     '''
-
     gt = session.query(GlobalToken).filter(token=token).first()
 
     session.delete(gt)
