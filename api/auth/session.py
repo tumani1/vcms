a2068172015307fa4958af6ef9ce4301b2a3bcba
Ivan <ikrivosheev@aaysm.com>
2014-07-15 10:11:54 +0400

    Add choices type for stream in admin. Add update last_visit for user.

--- api/auth/session.py
+++ api/auth/session.py
@@ -10,7 +10,9 @@ from utils.serializer import serialize
 @serialize
 def get(auth_user, session, **kwargs):
     sid, token, created = SessionToken.generate_token(auth_user.id, session)
-
+    auth_user.last_visit = datetime.datetime.utcnow()
+    session.add(auth_user)
+    session.commit()
     result = {'id': sid,
               'session_token': token,
               'expire': created + datetime.timedelta(minutes=TOKEN_LIFETIME)}
@@ -19,9 +21,8 @@ def get(auth_user, session, **kwargs):
 
 @need_authorization
 def delete(auth_user, session, **kwargs):
-
+    auth_user.last_visit = datetime.datetime.utcnow()
     st = session.query(SessionToken).filter(SessionToken.user_id == auth_user.id).first()
     st.is_active = False
-
-    session.add(st)
+    session.add_all((st, auth_user))
     session.commit()
--- utils/__init__.py
+++ utils/__init__.py
@@ -6,7 +6,7 @@ from utils.exceptions import NotAuthorizedException
 def need_authorization(func):
     def wraper(*args, **kwargs):
         if not ('auth_user' in kwargs) or kwargs['auth_user'] is None:
-            func_name = "{} in {}".format(func.func_name,func.func_code.co_filename)
+            func_name = "{} in {}".format(func.func_name, func.func_code.co_filename)
             raise NotAuthorizedException(func_name)
 
         return func(*args, **kwargs)
