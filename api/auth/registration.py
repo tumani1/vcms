7737f5a84866f19ad2ca1a7e15a9e6cd49ba640a
sergey.i <sipatov@aaysm.com>
2014-07-22 17:37:48 +0400

    api register

--- api/auth/registration.py
+++ api/auth/registration.py
@@ -3,16 +3,16 @@ from models.users.users import Users
 from utils.validation import validate_email, validate_int
 
 
-def post(session, firstname, lastname, email, pswd1, pswd2, city_id, **kwargs):
+def post(auth_user, session, firstname, lastname, email, pswd1, pswd2, city_id, **kwargs):
     try:
         email = validate_email(email)
-        city = validate_int(city_id, min_value=1)
+        city_id = validate_int(city_id, min_value=1)
         if pswd1 and pswd2 and pswd1 == pswd2:
             password = pswd1
         else:
             return {'error': u'Пароли не совпадают!'}
         if firstname and lastname:
-            user = Users(firstname=firstname, lastname=lastname, email=email, city_id=city, password=password)
+            user = Users(firstname=firstname, lastname=lastname, email=email, city_id=city_id, password=password)
             session.add(user)
             session.commit()
         else:
