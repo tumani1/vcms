22a0deff74a476d8b088bdba0ad10b7da2833f93
Denis Lyubimov <dlyubimov@aaysm.com>
2014-07-30 10:34:53 +0400

    Fixed test_runner. Added ability for next_tv_clear to run silent

--- utils/next_tv_clear.py
+++ utils/next_tv_clear.py
@@ -9,24 +9,23 @@ from utils.connection import db_connect, create_session
 parser = argparse.ArgumentParser(description='Clear database for NextTV service')
 parser.add_argument('-t', '--type', dest='type', action='store_true', default='postgresql',
                     help='Run server with test params')
+parser.add_argument('-y','--always-yes', dest='isyes', action='store_true', default=False)
 
 args = parser.parse_args()
 
 
-while True:
+if  not args.isyes:
     result = raw_input("Вы уверенны, что хотите очистить базу данных?(Yes/No)")
+else:
+    result = 'N'
+if args.isyes or result in ('y', 'Y', 'yes', 'Yes'):
+    if not args.type in settings.DATABASE.keys():
+        raise Exception('Необходим ключ из конфига базы данных')
+        
+    print "Идет очистка базы данных..."
+    session = create_session(bind=db_connect(type=args.type))
+    session.connection().connection.set_isolation_level(0)
+    session.execute("DROP SCHEMA public CASCADE; CREATE SCHEMA public;")
+    session.connection().connection.set_isolation_level(1)
 
-    if result in ('y', 'Y', 'yes', 'Yes'):
-        if not args.type in settings.DATABASE.keys():
-            raise Exception('Необходим ключ из конфига базы данных')
 
-        print "Идет очистка базы данных..."
-        session = create_session(bind=db_connect(type=args.type))
-        session.connection().connection.set_isolation_level(0)
-        session.execute("DROP SCHEMA public CASCADE; CREATE SCHEMA public;")
-        session.connection().connection.set_isolation_level(1)
-
-        break
-
-    elif result in ('n', 'N', 'no', 'No'):
-        break
--- utils/test_runner.sh
+++ utils/test_runner.sh
@@ -2,10 +2,10 @@
 for e in $(nosetests -v --collect-only $1 2>&1 | pyp "p.split('(')[1].split(')')[0]|list(set(pp))|p|'.'.join(p.split('.')[:-1]) +':'+p.split('.')[-1]");
    do
     echo "Running nosetests " $e
-    python manage.py local_db_reset;
-    python manage.py syncdb;
+    python -m utils.next_tv_clear -y;
+    python -m utils.next_tv_sync;
     # Starting server process
-    python -m zerorpcserver.service & zerorpc_pid=$! ;
+    python -m zerorpcservices.rest_service & zerorpc_pid=$! ;
 
     nosetests -v --with-xunit --xunit-file=$(echo $e| pyp "'_'.join(p.split(':')[0].split('.')+p.split(':')[1:]) +'.xml'") $e;
 
