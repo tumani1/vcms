ba1a634879cabe4d2a79256c006a6114c9760321
dmitriy krasnovm <dkrasnov@aaysm.com>
2014-08-11 17:02:14 +0400

    поправлены сериализаторы m_chat_msg, m_user_short. Поправлен метод создания пользователей, теперь имена можно пронумероввывать

--- api/chat/chat_stream.py
+++ api/chat/chat_stream.py
@@ -5,13 +5,13 @@ from utils.validation import validate_mLimit, validate_int
 
 def get_chat_stream(id, **kwargs):
     chat_id = validate_int(id)
-    auth_user = kwargs.get('auth_user')
-    limit_arg = kwargs['query']['limit']
+    session = kwargs.get('session')
+    limit = kwargs['query']['limit']
 
-    limit, top = validate_mLimit(limit_arg)
-    cms = ChatMessages.objects.filter(chat_id=chat_id, user_id=auth_user.id).skip(top)
+    limit, top = validate_mLimit(limit)
+    cms = ChatMessages.objects.filter(chat_id=chat_id).skip(top)
 
     if limit:
         cms = cms.limit(limit)
 
-    return mChatMsgSerializer(cms, auth_user).get_data()
+    return mChatMsgSerializer(cms, session).get_data()
--- api/serializers/m_chat_msg.py
+++ api/serializers/m_chat_msg.py
@@ -1,17 +1,36 @@
+#coding:utf-8
 from api.serializers import mUserShort
+from models import Users
 
 
-class mChatMsgSerializer(object):
+class mChatMsgSerializer(object):  # TODO: просмотреть как вызываются sql запросы
 
-    def __init__(self, chat_messages, user):
+    def __init__(self, chat_messages, session):
         self.chat_messages = chat_messages
-        self.user = user
+        self.session = session
+        self.users = session.query(Users).filter(Users.id.in_((cm.user_id for cm in chat_messages)))
 
     def get_data(self):
         cms = []
         for cm in self.chat_messages:
+            user = self.users.filter(Users.id==cm.user_id).one()
             cms.append({'id': cm.id,
                         'created': cm.created,
                         'text': cm.text,
-                        'user': mUserShort(self.user).data})
+                        'user': mUserShort(user, session=self.session).data})
         return cms
+
+
+# def get_data(self):
+#         cms = []
+#         for cm in self.chat_messages:
+#             user = None
+#             for u in self.users:
+#                 if u.id == cm.user_id:
+#                     user = u
+#                     break
+#             cms.append({'id': cm.id,
+#                         'created': cm.created,
+#                         'text': cm.text,
+#                         'user': mUserShort(user, session=self.session).data})
+#         return cms
