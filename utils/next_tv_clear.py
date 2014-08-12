b27a1db1cfa71d1fad9ae4e04226276c5ac651bf
sergey.i <sipatov@aaysm.com>
2014-08-12 12:28:40 +0400

    добавил печать в скрипт очистки базы

--- utils/next_tv_clear.py
+++ utils/next_tv_clear.py
@@ -21,8 +21,10 @@ if args.isyes or result.lower() in ('y', 'yes', 'Yes', 'Y'):
     if not args.type in settings.DATABASE.keys():
         raise Exception(u'Необходим ключ из конфига базы данных'.encode('utf-8'))
 
-    print u'Идет очистка базы данных...'.encode('utf-8')
+    print u'Начало создания сессии...'.encode('utf-8')
     session = create_session(bind=db_connect(type=args.type))
+
+    print u'Очистка...'.encode('utf-8')
     session.connection().connection.set_isolation_level(0)
     session.execute("DROP SCHEMA public CASCADE; CREATE SCHEMA public;")
     session.connection().connection.set_isolation_level(1)
