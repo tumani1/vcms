c086bbbb3a99ad17276691bb6189ec68ed92cf7f
Eugene <eshangin@aaysm.com>
2014-08-13 14:41:42 +0400

    Тесты content

--- tests/node_tests/test_content.py
+++ tests/node_tests/test_content.py
@@ -1,16 +1,58 @@
 import unittest
 import requests
+from models import Base
+from settings import NODE
+from tests.fixtures import create
+from utils.connection import db_connect, create_session
+
+
+HOST = NODE['rest_ws_serv']['host']
+PORT = NODE['rest_ws_serv']['port']
+URL = 'http://{}:{}'.format(HOST, PORT)
+
+
+def setUpModule():
+    engine = db_connect().connect()
+    engine.execute("drop schema public cascade; create schema public;")
+    session = create_session(bind=engine)
+
+    # Create table
+    Base.metadata.create_all(bind=engine)
+
+    create(session)
+
+    engine.close()
 
 
 class ContentInfoTestCase(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        cls.engine = db_connect().connect()
+        cls.session = create_session(bind=cls.engine, expire_on_commit=False)
+
+        cls.h, cls.p = NODE['rest_ws_serv']['host'], NODE['rest_ws_serv']['port']
+        cls.fullpath = 'http://{}:{}'.format(cls.h, cls.p)
+
+        cls.req_sess = requests.Session()
+
     def test_get_info(self):
-        resp = requests.get('http://127.0.0.1:9977/content/1/info')
+        resp = ContentInfoTestCase.req_sess.get(ContentInfoTestCase.fullpath + '/content/1/info')
         self.assertEqual(resp.json(), 1)
 
 
 class ContentListTestCase(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        cls.engine = db_connect().connect()
+        cls.session = create_session(bind=cls.engine, expire_on_commit=False)
+
+        cls.h, cls.p = NODE['rest_ws_serv']['host'], NODE['rest_ws_serv']['port']
+        cls.fullpath = 'http://{}:{}'.format(cls.h, cls.p)
+
+        cls.req_sess = requests.Session()
+
     def test_get_list(self):
-        resp = requests.get('http://127.0.0.1:9977/content/list')
+        resp = ContentInfoTestCase.req_sess.get(ContentInfoTestCase.fullpath + '/content/list')
         self.assertEqual(resp.json(), 1)
