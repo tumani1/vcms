560ffebde2172f01132f587e1b6493e646f4eac2
sergey.i <sipatov@aaysm.com>
2014-08-13 17:12:31 +0400

    модели carts

--- models/eshop/carts/items_carts.py
+++ models/eshop/carts/items_carts.py
@@ -1,6 +1,15 @@
 # coding: utf-8
+from sqlalchemy import Column, Integer, DateTime, Float, ForeignKey
 from models import Base
 
 
 class ItemsCarts(Base):
     __tablename__ = 'items_carts'
+
+    id          = Column(Integer, primary_key=True)
+    carts_id    = Column(Integer, ForeignKey('carts.id'), nullable=False)
+    variant_id  = Column(Integer, ForeignKey('variants.id'), nullable=False)
+    cnt         = Column(Integer)
+    price       = Column(Float)
+    cost        = Column(Float)
+    added       = Column(DateTime)
--- models/eshop/carts/payments.py
+++ models/eshop/carts/payments.py
@@ -1,1 +1,15 @@
+# coding: utf-8
+from sqlalchemy import Column, Integer, DateTime, ForeignKey, String, Float
+from models import Base
 
+
+class Payments(Base):
+    __tablename__ = 'payments'
+
+    id         = Column(Integer, primary_key=True)
+    cart_id    = Column(Integer, ForeignKey('carts.id'), nullable=False)
+    status     = Column(Integer)
+    created    = Column(DateTime)
+    payed      = Column(DateTime)
+    pay_system = Column(String)
+    cost       = Column(Float)
