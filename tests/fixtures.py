7f00fbab5c2fe0d22f15d4f38109a5a172c7cece
Ivan <ikrivosheev@aaysm.com>
2014-08-19 10:54:16 +0400

    Исправлено создание данных для тестовой базы

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -19,10 +19,10 @@ def create_media_units(session):
 
 
 def create_media(session):
-    media1 = Media(title='media1', owner=1, title_orig='test_media1', description='test_desc1', created=datetime.datetime(2014,1,1,0,0,0,0, tzinfo=UTC))
-    media2 = Media(title='media2', owner=1, title_orig='test_media2', description='test_desc2', created=datetime.datetime(2014,2,1,0,0,0,0, tzinfo=UTC))
-    media3 = Media(title='media3', owner=1, title_orig='test_media3', description='test_desc3', created=datetime.datetime(2014,2,1,0,0,0,0, tzinfo=UTC))
-    media4 = Media(title='media4', owner=1, title_orig='test_media4', description='test_desc4', created=datetime.datetime(2014,2,1,0,0,0,0, tzinfo=UTC))
+    media1 = Media(title='media1', type_='v', owner=1, title_orig='test_media1', description='test_desc1', created=datetime.datetime(2014,1,1,0,0,0,0, tzinfo=UTC))
+    media2 = Media(title='media2', type_='v', owner=1, title_orig='test_media2', description='test_desc2', created=datetime.datetime(2014,2,1,0,0,0,0, tzinfo=UTC))
+    media3 = Media(title='media3', type_='v', owner=1, title_orig='test_media3', description='test_desc3', created=datetime.datetime(2014,2,1,0,0,0,0, tzinfo=UTC))
+    media4 = Media(title='media4', type_='v', owner=1, title_orig='test_media4', description='test_desc4', created=datetime.datetime(2014,2,1,0,0,0,0, tzinfo=UTC))
 
     m_in_u1 = MediaInUnit(media_id=1, media_unit_id=2)
     m_in_u2 = MediaInUnit(media_id=2, media_unit_id=1)
