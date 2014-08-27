e727c608406ea273339476cd10b2a265d72af12a
Eugene <eshangin@aaysm.com>
2014-08-27 15:22:11 +0400

    api carts

--- models/eshop/carts/carts.py
+++ models/eshop/carts/carts.py
@@ -1,11 +1,11 @@
 # coding: utf-8
 import datetime
-from models import Base
-from sqlalchemy import Column, Integer, DateTime, Float, ForeignKey, String
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, DateTime, Float, ForeignKey, and_
+from sqlalchemy import Column, Integer, DateTime, Float, ForeignKey, and_, String
 from sqlalchemy.orm import relationship, contains_eager
-from models import Base, ItemsCarts, Payments, ItemsExtras, ItemsObjects, UsersItems, CartLog
+from models import Base
+from models.eshop.carts.items_carts import ItemsCarts
+from models.eshop.carts.payments import Payments
+from models.eshop.carts.cart_log import CartLog
 
 
 class Carts(Base):
@@ -13,15 +13,15 @@ class Carts(Base):
 
     id         = Column(Integer, primary_key=True)
     user_id    = Column(Integer, ForeignKey('users.id'), nullable=False)
-    items_cnt  = Column(Integer)
+    items_cnt  = Column(Integer, nullable=True)
     status     = Column(String)
-    cost_total = Column(Float)
+    cost_total = Column(Float, nullable=True)
     created     = Column(DateTime, default=datetime.datetime.utcnow)
     updated    = Column(DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
 
     items_carts = relationship('ItemsCarts', backref='carts', cascade='all, delete')
     payments = relationship('Payments', backref='carts', cascade='all, delete')
-    log = relationship('CartLOg', backref='carts', cascade='all, delete')
+    log = relationship('CartLog', backref='carts', cascade='all, delete')
 
     @classmethod
     def tmpl_for_carts(cls, carts_id, session, user):
@@ -43,8 +43,8 @@ class Carts(Base):
         return query
 
     @classmethod
-    def get_cart_by_user_id(cls, session, user_id, **kwargs):
-        query = session.query(cls).filter(cls.user_id == user_id)
+    def get_cart_active_by_user_id(cls, session, user_id, **kwargs):
+        query = session.query(cls).filter(and_(cls.user_id == user_id, cls.status == 'active'))
         return query
 
     def __repr__(self):
