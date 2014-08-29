c07955e4af1aaf20584fbc344fd260f3a549d736
Ivan <ikrivosheev@aaysm.com>
2014-08-29 13:18:56 +0400

    Добавлены тесты для тестирования внутренего API

--- api/internal/render/auth.py
+++ api/internal/render/auth.py
@@ -5,7 +5,7 @@ from api.serializers import mUser, mSession
 
 @need_authorization
 def get_user_session(auth_user, session, **kwargs):
-    return mSession(instance=auth_user.session_token, session=session).data
+    return mSession(instance=auth_user.session_token[0], session=session).data
 
 
 @need_authorization
@@ -13,6 +13,5 @@ def get_auth_user(auth_user, session, **kwargs):
     return mUser(instance=auth_user, session=session).data
 
 
-@need_authorization
 def is_auth(auth_user, **kwargs):
     return not auth_user is None
--- zerorpcservices/internal_service.py
+++ zerorpcservices/internal_service.py
@@ -4,7 +4,7 @@ import zerorpc
 from geoip2 import database
 
 import settings as conf
-from api import rest_routes
+from api import internal_routes
 from base_service import BaseService
 
 
@@ -21,7 +21,7 @@ class ZeroRpcInternalApiService(BaseService):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--host', dest='host', default='127.0.0.1')
-    parser.add_argument('--port', dest='port', default=6600)
+    parser.add_argument('--port', dest='port', default=6601)
     parser.add_argument('--testdb', dest='testdb', action='store_true', default=False,
                     help='использование тестовой БД')
     namespace = parser.parse_args()
@@ -29,7 +29,7 @@ if __name__ == '__main__':
     if namespace.testdb:
         conf.DATABASE['postgresql'] = conf.DATABASE['test']  # переключение на тестовую БД
 
-    server = zerorpc.Server(ZeroRpcInternalApiService(rest_routes))
+    server = zerorpc.Server(ZeroRpcInternalApiService(internal_routes))
     server.bind("tcp://{host}:{port}".format(**vars(namespace)))
     print("ZeroRPC: Starting {0} at {host}:{port}".format(ZeroRpcInternalApiService.__name__, **vars(namespace)))
     server.run()
