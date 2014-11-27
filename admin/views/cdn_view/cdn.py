848997911f1aa8a53a5f13eb2bdd11e8a7984d81
tumani1 <itumanov@aaysm.com>
2014-11-27 11:03:59 +0300

    - правки по cdn

--- admin/views/cdn_view/cdn.py
+++ admin/views/cdn_view/cdn.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+
 from wtforms.fields import StringField
 
 from admin.views.base import SqlAlModelView
@@ -7,20 +8,37 @@ from models.cdn import CDN
 
 class CdnModelView(SqlAlModelView):
     model = CDN
-
     category = u'CDN'
     name = u'CDN'
 
-    column_labels = dict(name=u'Название', description=u'Описание',
-                         cdn_type=u'Тип', url=u'URL адресс',
-                         has_mobile=u'Для мобильных устройств',
-                         has_auth=u'Необходима авторизация',
-                         location_regxp=u'Регулярное вырожожение для локации', )
+    named_filter_urls = True
 
-    form_columns = column_list = ('name', 'has_mobile', 'has_auth', 'cdn_type',
-                                  'url', 'location_regxp', 'description', )
+    column_filters = (
+        'name', 'has_mobile', 'has_auth',
+    )
+
+    column_labels = dict(
+        name=u'Название',
+        description=u'Описание',
+        cdn_type=u'Тип',
+        url=u'URL адресс',
+        has_mobile=u'Для мобильных устройств',
+        has_auth=u'Необходима авторизация',
+        location_regxp=u'Регулярное вырожожение для локации',
+    )
+
+    column_list = (
+        'name', 'has_mobile', 'has_auth', 'cdn_type',
+        'url', 'location_regxp', 'description',
+    )
+
+    form_columns = (
+        'name', 'has_mobile', 'has_auth', 'cdn_type',
+        'url', 'location_regxp', 'description',
+    )
 
     form_excluded_columns = ('extras', 'media_locations', )
+
     form_overrides = dict(
         name=StringField,
     )
