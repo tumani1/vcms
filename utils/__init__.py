c09f3ce27fde90a967754dc7e6328dcdd737f666
Dmitriy Krasnov <dkrasnov@aaysm.com>
2014-12-02 19:11:28 +0300

    легкий рефактор декоратора @need_authorization

--- utils/__init__.py
+++ utils/__init__.py
@@ -5,7 +5,7 @@ from utils.exceptions import NotAuthorizedException
 def need_authorization(func):
 
     def wrapper(*args, **kwargs):
-        if not ('auth_user' in kwargs) or kwargs['auth_user'] is None:
+        if not kwargs.get('auth_user', None):
             raise NotAuthorizedException
         return func(*args, **kwargs)
 
