9610603dfe6728d8fdaede5ae44527023ee0f5f2
sergey.i <sipatov@aaysm.com>
2014-12-02 16:45:13 +0300

    таск 645, и первые 2 пункта таска 646

--- api/serializers/m_media_unit.py
+++ api/serializers/m_media_unit.py
@@ -1,4 +1,6 @@
 # coding: utf-8
+from api.serializers import mTopicSerializer
+from models.topics.topics import Topics
 from utils.serializer import DefaultSerializer
 from utils.common import datetime_to_unixtime as convert_date
 
@@ -7,20 +9,29 @@ __all__ = ['mMediaUnitsSerializer']
 
 class mMediaUnitsSerializer(DefaultSerializer):
 
-    __read_fields = {
-        'id': '',
-        'title': '',
-        'title_orig': '',
-        'description': '',
-        'releasedate': '',
-        'enddate': '',
-        'batch': '',
-        'prev': '',
-        'next': '',
-        'relation': '',
-    }
-
     def __init__(self, **kwargs):
+        self.__read_fields = {
+            'id': '',
+            'title': '',
+            'title_orig': '',
+            'description': '',
+            'releasedate': '',
+            'enddate': '',
+            'batch': '',
+            'prev': '',
+            'next': '',
+            'relation': '',
+            'topic': '',
+        }
+        self.small = False
+        if 'small' in kwargs:
+            if kwargs['small']:
+                self.small = True
+                not_small_fields = ['description', 'releasedate', 'enddate', 'batch', 'prev', 'next']
+                cl = '_{0}__read_fields'.format(self.__class__.__name__)
+                for field in not_small_fields:
+                    del getattr(self, cl)[field]
+
         self.fields = self.__read_fields
         super(mMediaUnitsSerializer, self).__init__(**kwargs)
 
@@ -60,3 +71,7 @@ class mMediaUnitsSerializer(DefaultSerializer):
             if user_media_unit[0].watched:
                 relation.update(watched=convert_date(user_media_unit[0].watched))
         return relation
+
+    def transform_topic(self, instance, **kwargs):
+        topic = Topics.get_topics_by_name(self.user, instance.topic.name, self.session)
+        return mTopicSerializer(user=self.user, session=self.session, instance=topic, small=self.small).data
