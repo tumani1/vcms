1ae4037f5e28bf5d46ffc5afa7ec46031e332078
Eugene <eshangin@aaysm.com>
2014-12-03 17:52:36 +0400

    Авторизация через контакт рабочая

--- api/auth/__init__.py
+++ api/auth/__init__.py
@@ -4,21 +4,16 @@ from api.auth import revoke
 from api.auth import session
 from api.auth import registration_email
 from api.auth import registration_phone
+from api.auth.social_auth import vk_oauth
 
-from api.auth.login import post
-from api.auth.revoke import revoke
-from api.auth.session import get, delete
-
-from api.auth.social_auth.vk_oauth import get as vk_oauth2_get
-from api.auth.social_auth.vk_oauth import complete_get as vk_oauth2_complete
 
 
 routing = (
-    # # (r'^login$', {'post': login.post}),
-    # # (r'^session$', {'get': session.get, 'delete': session.delete}),
-    # # (r'^revoke$', {'get': revoke.revoke}),
-    # # (r'^register$', {'post': registration_email.post}),
-    # (r'^register_phone$', {'post': registration_phone.post})
-    (r'^login/vk-oauth2$', {'get': vk_oauth2_get}),
-    (r'^complete/vk-oauth2$', {'get': vk_oauth2_complete}),
+    (r'^login$', {'post': login.post}),
+    (r'^session$', {'get': session.get, 'delete': session.delete}),
+    (r'^revoke$', {'get': revoke}),
+    (r'^register$', {'post': registration_email.post}),
+    (r'^register_phone$', {'post': registration_phone.post}),
+    (r'^login/vk-oauth2$', {'get': vk_oauth.get}),
+    (r'^complete/vk-oauth2$', {'get': vk_oauth.complete_get}),
 )
