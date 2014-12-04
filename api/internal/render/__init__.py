210e5cae325986f7052546b86f38fc5586789a23
Ivan <ikrivosheev@aaysm.com>
2014-12-04 19:03:09 +0300

    Удалена лишняя API

--- api/internal/render/__init__.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-import auth
-
-
-routing = (
-    (r'^info/session$', {'get': auth.get_user_session}),
-    (r'^info/user$', {'get': auth.get_auth_user}),
-    (r'^auth/check$', {'get': auth.is_auth}),
-)
