22c9234dd0dd6d446e34acf1f06605016d4dc364
Dmitriy Krasnov <dkrasnov@aaysm.com>
2014-12-10 13:54:41 +0300

    расширен метод сохранения аватарки, рефактор метода изменения пароля

--- api/user/user_avatar.py
+++ api/user/user_avatar.py
@@ -1,6 +1,8 @@
 #coding:utf-8
+from os.path import join
 from utils import need_authorization
 from models.extras import Extras
+from models.users import UsersExtras
 from models.cdn import CDN
 from models.extras.constants import APP_EXTRA_TYPE_IMAGE
 from utils.exceptions import RequestErrorException
@@ -13,10 +15,16 @@ def post(session, auth_user, **kwargs):
 
     if filename:
         cdn = session.query(CDN).first()
+        location = join(str(auth_user.id), filename)
         if cdn:
-            e = Extras(type=APP_EXTRA_TYPE_IMAGE, title=filename, title_orig=filename,
-                            description='avatar', location=auth_user.id, cdn_name=cdn.name)
+            e = session.query(Extras).filter_by(type=APP_EXTRA_TYPE_IMAGE, location=location, cdn_name=cdn.name).first()
+            if not e:
+                e = Extras(type=APP_EXTRA_TYPE_IMAGE, title='', title_orig='',
+                           description='', location=location, cdn_name=cdn.name)
             session.add(e)
+            session.commit()
+            ue = UsersExtras(user_id=auth_user.id, extra_id=e.id)
+            session.add(ue)
             session.commit()
         else:
             raise RequestErrorException
