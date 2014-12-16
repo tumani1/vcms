e4a081b91df3e252ce61e690acbcba279cc558d5
Eugene <eshangin@aaysm.com>
2014-12-16 12:55:32 +0400

    Сгенерировал сигнатуру дляtwittera

--- api/login/tw_oauth.py
+++ api/login/tw_oauth.py
@@ -136,17 +136,17 @@ def complete_get(auth_user, session, **kwargs):
     key = u'L8ejYRiZZOgUz0jvalLU1xGdm7jwjrrfMJ8U5FtexFQBt74DBx&' + oauth_token_secret
     sig = sign_hmac_sha1(base_string, key, None)
 
-    headers = [
-        (u'oauth_consumer_key', u'u7Vdu6ScezMQlpcCog3t7g7xx'),
-        (u'oauth_nonce', nonce),
-        (u'oauth_signature', sig),
-        (u'oauth_signature_method', u'HMAC-SHA1'),
-        (u'oauth_timestamp', unicode(ts)),
-        (u'oauth_token', unicode(oauth_token)),
-        (u'oauth_version',  u'1.0'),
-        (u'screen_name',  unicode(screen_name))
-    ]
-    headers = prepare_headers(headers)
-    response = requests.get(u'https://api.twitter.com/1.1/users/show.json', headers=headers)
+    headers = {
+        u'oauth_consumer_key': u'u7Vdu6ScezMQlpcCog3t7g7xx',
+        u'oauth_nonce': nonce,
+        u'oauth_signature': sig,
+        u'oauth_signature_method': u'HMAC-SHA1',
+        u'oauth_timestamp': unicode(ts),
+        u'oauth_token': unicode(oauth_token),
+        u'oauth_version': u'1.0',
+        u'screen_name': unicode(screen_name)
+    }
+    headers = urlencode(headers)
+    response = requests.get(u'https://api.twitter.com/1.1/users/show.json?'+headers)
 
     return {'text': response.content}
